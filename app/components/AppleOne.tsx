4b7d5d36d679e35c90bf33f4341174db7cce91db
NeonNature <mustachemaung@gmail.com>
2023-11-05 09:26:03 +0630

    feat: add skeleton structure

---
+++ app/components/AppleOne.tsx
@@ -0,0 +1,3 @@
+export default function AppleOne() {
+  return <div />
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
-import styles from './page.module.css'
+import Navbar from './components/Navbar'
+import LandingVideo from './components/LandingVideo'
+import Carousel from './components/Carousel'
+import Lineup from './components/Lineup'
+import Devices from './components/Devices'
+import Faq from './components/Faq'
+import AppleOne from './components/AppleOne'
+import Footer from './components/Footer'
 
 export default function Home() {
-  return <main className={styles.main}></main>
+  return (
+    <main>
+      <Navbar />
+      <LandingVideo />
+      <Carousel />
+      <Lineup />
+      <Devices />
+      <Faq />
+      <AppleOne />
+      <Footer />
+    </main>
+  )
 }
